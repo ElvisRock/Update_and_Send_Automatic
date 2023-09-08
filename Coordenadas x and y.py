ac7be34abb4d0f9530dfe63aca35b6a9fd0541d5
Elvis Monteiro <93358171+ElvisRock@users.noreply.github.com>
2023-09-08 18:20:51 -0300

    Coordenadas X and Y

---
+++ Coordenadas x and y.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Encontrar as coordenadas da tela
+
+# <font color ='grey'> Para encontrar as coordenada exatas, basta rodar este script. O módulo 'time', permite adicionar tempo em segundos</font>
+
+# In[9]:
+
+
+# Imports
+import pyautogui
+import time 
+
+time.sleep(2)
+pyautogui.position()
+
